3bfe8468261f5da4aa818873e8224336d10d2772
张朝伟 <zcw_lwen@sina.cn>
2016-10-11 17:44:48 +0800

    php脚本传值

---
+++ passValue/one.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abai
+ * Date: 2016/10/9
+ * Time: 下午2:39
+ */
+
+$aa = $argv[0];
+$bb = $argv[1];
+
+echo $aa;
+var_dump($bb);
---
+++ passValue/two.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abai
+ * Date: 2016/10/9
+ * Time: 下午2:39
+ */
+
+$info = array();
+$info['a'] = 'aaa';
+$info['b'] = 'bbb';
+
+$cmd = "php -f two.php $info";
